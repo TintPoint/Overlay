381d47c3fc3081192354082e885594f8afc4d36b
Justin Jia <justin.jia.developer@gmail.com>
2016-07-24 17:19:21 -0700

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  Overlay
+//
+//  Created by Justin Jia on 7/24/16.
+//  Copyright © 2016 TintPoint. MIT license.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "Overlay"
+)

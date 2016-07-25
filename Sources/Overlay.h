b92af1a21db8d6b8d1537285907413c87b6f8409
Justin Jia <justin.jia.developer@gmail.com>
2016-07-24 18:29:30 -0700

    Add Xcode project

---
+++ Sources/Overlay.h
@@ -0,0 +1,12 @@
+//
+//  Overlay.h
+//  Overlay
+//
+//  Created by Justin Jia on 7/24/16.
+//  Copyright © 2016 TintPoint. MIT license.
+//
+
+@import UIKit;
+
+FOUNDATION_EXPORT double OverlayVersionNumber;
+FOUNDATION_EXPORT const unsigned char OverlayVersionString[];

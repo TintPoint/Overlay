c6ccf684fc6d83fc715322f5b15e0e114484cea2
Justin Jia <justin.jia.developer@gmail.com>
2016-08-21 23:52:35 +0800

    Add documentation comment for customizable

--- Sources/Customizable.swift
+++ Sources/Customizable.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
+/// A protocol that describes a view that can be customized.
 public protocol ViewCustomizable {
 
+    /// Updates the view's appearance.
+    ///
+    /// Your custom view can override this method to customize other properties.
+    /// You need to call the super implementation at some point during your own `refresh` method.
     func refresh()
 
 }
@@ -111,28 +116,36 @@ extension ViewCustomizable {
     
 }
 
-public protocol ViewFocusable {
-
-    var isFocused: Bool { get }
-
-}
-
+/// A protocol that describes a view that can be disabled.
+/// - SeeAlso: `ViewSelectable`, `ViewHighlightable`, `ViewFocusable`
 public protocol ViewDisable {
 
     var isEnabled: Bool { get }
 
 }
 
+/// A protocol that describes a view that can be selected.
+/// - SeeAlso: `ViewDisable`, `ViewHighlightable`, `ViewFocusable`
 public protocol ViewSelectable {
 
     var isSelected: Bool { get }
 
 }
 
+/// A protocol that describes a view that can be highlighted.
+/// - SeeAlso: `ViewDisable`, `ViewSelectable`, `ViewFocusable`
 public protocol ViewHighlightable {
 
     var isHighlighted: Bool { get }
 
+}
+
+/// A protocol that describes a view that can be focused.
+/// - SeeAlso: `ViewDisable`, `ViewSelectable`, `ViewHighlightable`
+public protocol ViewFocusable {
+    
+    var isFocused: Bool { get }
+    
 }
 
 extension UIBarItem: ViewDisable { }
